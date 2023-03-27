e44f98cd20451c507876e1e0dd4256eddfcd798a
reinaldoper <reinaldoper83@gmail.com>
2023-03-26 21:24:01 -0400

    feat: aplictivo de search de musicas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { search } from './service/feths';
 
+const album = 'Pesquise por bandas de musicas'
 function App() {
+  const [listMusic, setListMusic] = useState([]);
+  const [name, setName] = useState('');
+
+  const handleChange = async (event) => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '66a8b6d81emsh798551a4ab13e56p1e20b9jsna74896fddd71',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+      }
+    };
+    const result = await search(name, options);
+    setListMusic(result.data);
+  };
+  console.log(listMusic);
+  const listRenderMusics = listMusic.map((music) => (
+    <div key={ music.id } className='music-render'>
+      <p>{ music.title }</p>
+      <img src={ music.album.cover} alt={ music.title } className='img-music' />
+      <audio id="player"  controls="controls">
+        <source src={music.preview} type="audio/mp3" />
+        seu navegador não suporta HTML5
+      </audio>
+    </div>
+  ));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>{listMusic.length > 0 ? <h2>{listMusic[0].artist.name}</h2> : album}</li>
+        <li>{listMusic.length > 0 ? <img src={listMusic[0].artist.picture} alt='img' /> : null}</li>
+        <li><input type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='search' /></li>
+        <li><button type='button' onClick={handleChange}>search</button></li>
+      </ul>
+      {listMusic.length > 0 ? <div className='control-music'>{listRenderMusics}</div> : null}
     </div>
   );
 }

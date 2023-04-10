c81865e77b367cc61c089d581eff0bfca842a0c1
reinaldoper <reinaldoper83@gmail.com>
2023-04-09 21:40:48 -0400

    feat: mais um teste

--- src/App.js
+++ src/App.js
@@ -21,7 +21,10 @@ function App() {
   const handleChange = async () => {
     const options = {
       method: 'GET',
-      headers: '.env',
+      headers: {
+        'X-RapidAPI-Key': '66a8b6d81emsh798551a4ab13e56p1e20b9jsna74896fddd71',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+      }
     };
     if (name.length === 0) {
       alert('Please provide a name');
